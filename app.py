4be060324c4cc15ca3a4cf31eb6ab1e61127b813
joachimnormark <trop.dk@gmail.com>
2026-02-11 19:57:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ COLORS = {
     "Supervision": "#DC143C",
     "Junior": "#228B22",
     # Member type colors (til Tabel 8)
-    "Praktiserende læger": "#1f77b4",
+    "Praktiserende læger": "#34eb3a",
     "§-ansatte, vikarer mv": "#ff7f0e",
     "Uddannelseslæger": "#2ca02c",
     "Ej registreret": "#9467bd",
